58eb8ae564cbd393622ef75e306faf17f969209c
Gregory <necrondragonss@gmail.com>
2026-03-25 00:53:02 -0300

    foram implementadas as dependecias, e foi conectado o banco de dados mediante jdbc

--- src/main/java/com/github/Gregorys2s/conection.java
+++ src/main/java/com/github/Gregorys2s/conection.java
@@ -1,4 +1,26 @@
 package com.github.Gregorys2s;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class conection {
+    public static Connection conectar()
+    {
+        String url = "jdbc:postgresql://localhost:5432/systemsos";
+        String usuario = "postgres";
+        String senha = "admin";
+
+        try {
+            Connection conectado = DriverManager.getConnection(url,usuario,senha);
+            System.out.println("Conectado com sucesso!");
+            return conectado;
+        }catch(SQLException e)
+        {
+            System.out.println("Erro ao conectar");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
