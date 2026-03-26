b8c4d30fe850df0e9c3f63f45873ae97835b1f1c
Moraes-dias <pedro.m.dias.d.moraes@gmail.com>
2026-03-26 12:09:32 -0300

    update Usuario mudei pra lombok pra ficar mais limpo

--- src/main/java/com/github/Gregorys2s/Usuario.java
+++ src/main/java/com/github/Gregorys2s/Usuario.java
@@ -1,10 +1,17 @@
 package com.github.Gregorys2s;
 
 import jakarta.persistence.*;
-
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 import java.time.LocalDate;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor//isso é obrigatorio pro JPA funcionar
+@AllArgsConstructor//pode ser removido vc decide gregory
 public class Usuario {
 
     @Id
@@ -15,52 +22,4 @@ public class Usuario {
     private String profissao;
     private String telefone;
     private String nome;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public LocalDate getDataDeNascimento() {
-        return dataDeNascimento;
-    }
-
-    public void setDataDeNascimento(LocalDate dataDeNascimento) {
-        this.dataDeNascimento = dataDeNascimento;
-    }
-
-    public String getProfissao() {
-        return profissao;
-    }
-
-    public void setProfissao(String profissao) {
-        this.profissao = profissao;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 }
