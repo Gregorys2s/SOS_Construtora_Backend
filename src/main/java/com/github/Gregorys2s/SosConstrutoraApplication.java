4a5d9cf1f20a82f41ecc1eefe9ad5a28ca327d20
Moraes-dias <pedro.m.dias.d.moraes@gmail.com>
2026-03-26 14:10:38 -0300

    update SosConstrutoraApplication agr é main

--- src/main/java/com/github/Gregorys2s/SosConstrutoraApplication.java
+++ src/main/java/com/github/Gregorys2s/SosConstrutoraApplication.java
@@ -5,4 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SosConstrutoraApplication {
+      public static void main(String[] args) {
+            SpringApplication.run(SosConstrutoraApplication.class, args);
+        }
 }
